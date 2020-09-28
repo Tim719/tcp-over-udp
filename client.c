e6bc61ba1771e39de44933941a3d85884ab6adff
Tim719 <11026713+Tim719@users.noreply.github.com>
2020-09-28 17:59:31 +0200

    3 ways handshake fonctionnel

--- client.c
+++ client.c
@@ -5,60 +5,125 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define RCVSIZE 1024
 
-int main (int argc, char* argv[]) {
+int tcp_over_udp_connect(int fd, struct sockaddr_in *server)
+{
+  char msg[32];
+  int n;
+  socklen_t server_len = sizeof(struct sockaddr);
+  printf("Sending SYN\n");
+
+  n = sendto(fd, "SYN", 3, 0, (struct sockaddr *) server, server_len);
+  if (n < 0)
+  {
+    perror("Error sending SYN packet\n");
+    return -1;
+  }
+
+  printf("SYN sent\n");
+
+  memset(msg, 0, 32);
+  n = recvfrom(fd, &msg, 32, 0, (struct sockaddr *) server, &server_len);
+
+  if (n < 0) {
+    perror("Error receiving SYN-ACK");
+    return -1;
+  }
+
+  if (strncmp(msg, "SYN-ACK", 7) != 0)
+  {
+    perror("SYN-ACK not received\n");
+    return -1;
+  }
+
+  printf("SYN-ACK received\n");
+
+  char *data_port = malloc(6);
+
+  strncpy(data_port, msg + 8, 6);
+
+  printf("Sending ACK\n");
+  n = sendto(fd, "ACK", 3, 0, (struct sockaddr *) server, server_len);
+  if (n < 0)
+  {
+    perror("Error sending ACK packet\n");
+    return -1;
+  }
+
+  printf("ACK sent\n");
+  return atoi(data_port);
+}
+
+int main(int argc, char *argv[])
+{
 
   struct sockaddr_in adresse;
   int port = 1234;
   int valid = 1;
-  char msg[RCVSIZE];
-  char blanmsg[RCVSIZE];
+  // int recvsize = 0;
+  // char msg[RCVSIZE];
+  // char blanmsg[RCVSIZE];
 
   //create socket
   int server_desc = socket(AF_INET, SOCK_DGRAM, 0);
 
   // handle error
-  if (server_desc < 0) {
+  if (server_desc < 0)
+  {
     perror("cannot create socket\n");
     return -1;
   }
 
   setsockopt(server_desc, SOL_SOCKET, SO_REUSEADDR, &valid, sizeof(int));
 
-  adresse.sin_family= AF_INET;
-  adresse.sin_port= htons(port);
-  adresse.sin_addr.s_addr= htonl(INADDR_LOOPBACK);
-
+  adresse.sin_family = AF_INET;
+  adresse.sin_port = htons(port);
+  adresse.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
   // connect
-  int rc = connect(server_desc, (struct sockaddr *) &adresse, sizeof(adresse));
-  printf("value of connect is: %d\n", rc);
+  //int rc = connect(server_desc, (struct sockaddr *)&adresse, sizeof(adresse));
+  //printf("value of connect is: %d\n", rc);
 
   // handle error
-  if (rc < 0) {
+  /*
+  if (rc < 0)
+  {
     perror("connect failed\n");
     return -1;
   }
+  */
 
+  int data_port = tcp_over_udp_connect(server_desc, &adresse);
 
+  if (data_port < 0) {
+    printf("Error opening connection\n");
+    return -1;
+  }
+
+  printf("Data port: %d\n", data_port);
+
+  
+  /*
   int cont= 1;
   while (cont) {
     memset(&msg, 0, RCVSIZE);
     memset(&blanmsg, 0, RCVSIZE);
     printf("Entrez votre message\n");
     fgets(msg,255,stdin);
-    int n = write(server_desc, msg, strlen(msg));
-    if (n < 0)
+    recvsize = write(server_desc, msg, strlen(msg));
+    if (recvsize < 0)
     {
          perror("ERROR writing to socket");
     }
     fflush(stdin);
-    int nr = read(server_desc, &blanmsg, RCVSIZE);
-    printf("server(%d)> %s\n", nr, blanmsg);
+    recvsize = read(server_desc, &blanmsg, RCVSIZE);
+    printf("server(%d)> %s\n", recvsize, blanmsg);
   }
+  */
 
-close(server_desc);
-return 0;
+  close(server_desc);
+  return 0;
 }
--- server.c
+++ server.c
@@ -10,55 +10,123 @@
 
 #define RCVSIZE 1024
 
-int main (int argc, char *argv[]) {
+int tcp_over_udp_accept(int fd, int data_port, struct sockaddr_in *client)
+{
+  printf("Waiting for connection\n");
+  char buffer[32];
+  int n;
+  socklen_t client_size = sizeof(struct sockaddr);
 
+  char* SYN_ACK = malloc(14);
+  sprintf(SYN_ACK, "SYN-ACK-%d", data_port);
+
+  memset(buffer, 0, 32);
+  n = recvfrom(fd, &buffer, 32, 0, (struct sockaddr *) client, &client_size);
+
+  printf("Message received : %s\n", buffer);
+
+  if (strcmp(buffer, "SYN") != 0)
+  {
+    perror("Connection must start with SYN\n");
+    return -1;
+  }
+  printf("SYN received\n");
+
+  n = sendto(fd, SYN_ACK, strlen(SYN_ACK), 0, (struct sockaddr *) client, client_size);
+  if (n < 0)
+  {
+    perror("Unable to send SYN-ACK\n");
+    return -1;
+  }
+  printf("SYN-ACK sent\n");
+
+  memset(buffer, 0, 32);
+
+  n = recvfrom(fd, &buffer, 32, 0, (struct sockaddr *) client, &client_size);
+  if (strcmp(buffer, "ACK") != 0)
+  {
+    perror("ACK not received\n");
+    return -1;
+  }
+  printf("ACK received. Connected\n");
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  printf("Henlo\n");
   struct sockaddr_in adresse, client;
-  socklen_t client_size = sizeof(client);
   int port = 0;
-  int msgSize = 0;
-  char buffer[RCVSIZE];
+  int data_port = 0;
 
-  if (argc != 2) {
-    printf("USAGE: server.c <port>\n");
+  if (argc != 3)
+  {
+    printf("USAGE: server.c <control_port> <data_port>\n");
     return 0;
   }
 
   port = atoi(argv[1]);
+  data_port = atoi(argv[2]);
 
-  if (port < 0) {
+  if (port < 0 || data_port < 0)
+  {
     printf("Port number must be greater than 0\n");
   }
 
   //create socket
   int server_udp = socket(AF_INET, SOCK_DGRAM, 0);
   int optval = 1;
-	setsockopt(server_udp, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
+  setsockopt(server_udp, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 
   //handle error
-  if (server_udp < 0) {
+  if (server_udp < 0)
+  {
     perror("Cannot create socket\n");
     return -1;
   }
 
+  printf("Binding address...\n");
 
-  adresse.sin_family= AF_INET;
-  adresse.sin_port= htons(port);
-  adresse.sin_addr.s_addr= htonl(INADDR_ANY);
+  adresse.sin_family = AF_INET;
+  adresse.sin_port = htons(port);
+  adresse.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (bind(server_udp, (struct sockaddr*) &adresse, sizeof(adresse)) == -1) {
+  if (bind(server_udp, (struct sockaddr *)&adresse, sizeof(adresse)) == -1)
+  {
     perror("UDP Bind failed\n");
     close(server_udp);
     return -1;
   }
 
+  printf("Connecting...\n");
 
+  // Serveur UDP
+  int status = tcp_over_udp_accept(server_udp, data_port, &client);
+
+  if (status == 0) {
+    printf("Everything is fine my friend\n");
+  }
+
+  /*
   while (1) {
     printf("Accepting connections on port %d\n", port);
     
     // Serveur UDP
     memset(buffer,0,RCVSIZE);
-    //int msgSize = read(server_udp, buffer,RCVSIZE);
+
     msgSize = recvfrom(server_udp, &buffer, RCVSIZE, 0, (struct sockaddr *) &client, &client_size);
+
+    if (strcmp(buffer, "SYN")) {
+      printf("SYN received\n");
+      msgSize = sendto(server_udp, &SYN_ACK, strlen(SYN_ACK), 0, (struct sockaddr *) &client, client_size); 
+      printf("SYN-ACK sent: %s\n", SYN_ACK);
+      memset(buffer,0,RCVSIZE);
+      msgSize = recvfrom(server_udp, &buffer, RCVSIZE, 0, (struct sockaddr *) &client, &client_size);
+      if (strcmp(buffer, "ACK")) {
+        printf("ACK received. Connected");
+      }
+    }
 
     if (msgSize < 0) {
       break;
@@ -70,7 +138,8 @@ int main (int argc, char *argv[]) {
       printf("%d bytes sent\n", msgSize);
     }
   }
-  
+  */
+
   printf("Closing UDP server\n");
   close(server_udp);
   return 0;
